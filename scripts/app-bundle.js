951ff26be28a211b31d50a1aa710c12ce59345a8
Rob Eisenberg <rob@bluespire.com>
2016-11-01 22:46:59 -0700

    chore(all): initial project setup

---
+++ scripts/app-bundle.js
@@ -0,0 +1,51 @@
+define('app',["require", "exports"], function (require, exports) {
+    "use strict";
+    var App = (function () {
+        function App() {
+            this.message = 'Hello World!';
+        }
+        return App;
+    }());
+    exports.App = App;
+});
+
+define('environment',["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = {
+        debug: true,
+        testing: true
+    };
+});
+
+define('main',["require", "exports", './environment'], function (require, exports, environment_1) {
+    "use strict";
+    Promise.config({
+        warnings: {
+            wForgottenReturn: false
+        }
+    });
+    function configure(aurelia) {
+        aurelia.use
+            .standardConfiguration()
+            .feature('resources');
+        if (environment_1.default.debug) {
+            aurelia.use.developmentLogging();
+        }
+        if (environment_1.default.testing) {
+            aurelia.use.plugin('aurelia-testing');
+        }
+        aurelia.start().then(function () { return aurelia.setRoot(); });
+    }
+    exports.configure = configure;
+});
+
+define('resources/index',["require", "exports"], function (require, exports) {
+    "use strict";
+    function configure(config) {
+    }
+    exports.configure = configure;
+});
+
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <h1>${message}</h1>\n</template>\n"; });
+//# sourceMappingURL=app-bundle.js.map
