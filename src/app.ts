93b8bf510d829857c550bc9543ae0fe6cc30242a
Rob Eisenberg <rob@bluespire.com>
2016-11-06 00:39:37 -0700

    feat(all): restructure the shell

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
+import {AureliaUX} from 'aurelia-ux';
+import {inject} from 'aurelia-dependency-injection';
+
+@inject(AureliaUX)
 export class App {
+  router;
+
+  constructor(ux) {
+    ux.design.primary = '#704794';
+    ux.design.accent = '#e62787';
+  }
+
   configureRouter(config, router) {
+    this.router = router;
     config.map([
-      { route: '', redirect: 'home' },
-      { route: 'home', moduleId: './home', name: 'home' },
+      { route: '', redirect: 'introduction' },
+      { route: 'introduction', moduleId: './home', name: 'introduction', title: 'Introduction' },
 
-      { route: 'swatches', moduleId: './core-features/swatches', name: 'swatches' },
-      { route: 'theming', moduleId: './core-features/theming', name: 'theming' },
+      { route: 'swatches', moduleId: './core-features/swatches', name: 'swatches', title: 'Swatches', nav: true },
+      { route: 'theming', moduleId: './core-features/theming', name: 'theming', title: 'Theming', nav: true },
 
-      { route: 'buttons', moduleId: './components/buttons', name: 'buttons' }
+      { route: 'buttons', moduleId: './components/buttons', name: 'buttons', title: 'Buttons', nav: true }
     ]);
   }
 }
